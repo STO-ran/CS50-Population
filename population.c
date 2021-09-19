34d6464c2230042dea587b715ddffe215e1096a6
STO-ran <81787121+STO-ran@users.noreply.github.com>
2021-09-19 13:54:38 -0700

    Update population.c

--- population.c
+++ population.c
@@ -4,26 +4,33 @@
 int main(void)
 {
     // Prompt for start size
-    int n;
+    int start;
     do
     {
-        n = get_int("Enter the initial population size: ");
+        start = get_int("Enter the initial population size: ");
     }
-    while (n < 9);
+    while (start < 9);
 
     // Prompt for end size
-    int e;
+    int end;
     do
     {
-        e = get_int("Enter the final population size: ");
+        end = get_int("Enter the final population size: ");
     }
-    while (e < 9);
+    while (end < 9);
 
     // Calculate number of years until we reach threshold
-    int t = e % (n + (n / 3) - (n / 4));
+    int years=0;
+    while (end > start)
+    {
+
+        start = (start + (start / 3) - (start / 4));
+        years++;
+
+    }
 
     // TODO: Print number of years
-    printf("The initial population size is: %i\n", n);
-    printf("The final population size is: %i\n", e);
-    printf("The number of years to attain the final population size is: %i\n", t);
+    printf("The initial population size is: %i\n", start);
+    printf("The final population size is: %i\n", end);
+    printf("The number of years to attain the final population size is: %i\n", years);
 }
